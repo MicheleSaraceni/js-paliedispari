2d11461ea57a6b26a60cec3b4ad6ef354b0291ae
MicheleSaraceni <michele.saraceni23@email.com>
2024-11-05 14:47:11 +0100

    added exercise "Pari e Dispari"

--- script.js
+++ script.js
@@ -17,13 +17,63 @@ Buon lavoro e buon divertimento!
 "use strict";
 console.clear();
 
+/*********** FUNZIONI ***********/
+
+/**
+ * RANDOM: Genera un numero random da un valore minimo ad un valore massimo e lo ritorna in un valore numerico intero.
+ * @param {number} min Valore minimo del random
+ * @param {number} max Valore massimo del random
+ */
+function random(min, max){
+    const nrandom = Math.floor(Math.random() * (max - min + 1)) + min;
+    return nrandom;
+}
+
+/**
+ * PARI O DISPARO: Ritorna la parola "True" o "False"
+ * @param {number} n 
+ * @returns 
+ * 
+ */
+function evenodd(n){
+    let paridispari = "dispari";
+    if (n % 2 === 0){
+        paridispari = "pari";
+    }
+    return paridispari;
+}
+
 /*Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
 
+
+
+
 /*Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).*/
+
+//Chiedo all'utente numero e pari o dispari
+let sceltaplayer = prompt("Scegli pari o dispari");
+let nplayer = parseInt(prompt("Scegli numero da 1 a 5"));
+
+//Richiamo la funzione random e gli do valore min 1 max 5 per ottenere il numero del computer
+let ncomputer = random(1, 5);
+console.log ("Il numero del pc è: " + ncomputer);
+
+if (evenodd(nplayer) === sceltaplayer && evenodd(ncomputer) !== sceltaplayer){
+    console.log ("Hai vinto!");
+} else if (evenodd(nplayer) !== sceltaplayer && evenodd(ncomputer) === sceltaplayer){
+    console.log ("Hai perso!");
+} else if (evenodd(nplayer) === sceltaplayer && evenodd(ncomputer) === sceltaplayer){
+    console.log ("Pareggio, avete indovinato entrambi!");
+} else {
+    console.log ("Avete perso entrambi!");
+}
+
+
+
 
 /*Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
